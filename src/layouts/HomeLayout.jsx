87548db4442be9955dd3c6bd03e39c69948a053b
Protima <protima.swe@gmail.com>
2025-02-07 18:21:11 +0800

    dragon new project set up and navbar section done

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,31 @@
+import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/layout-component/LeftNavbar";
+import Navbar from "../components/Navbar";
+
+
+const HomeLayout = () => {
+    return (
+        <div className="font-poppins">
+            <header>
+                <Header></Header>
+                <section className="w-11/12 mx-auto">
+                    <LatestNews></LatestNews>
+                </section>
+
+            </header>
+            <nav className="w-11/12 mx-auto py-2">
+                <Navbar></Navbar>
+            </nav>
+            <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+                <aside className="left col-span-3">
+                    <LeftNavbar></LeftNavbar>
+                </aside>
+                <section className="col-span-6">Main Navbar</section>
+                <aside className="col-span-3">Right Navbar</aside>
+            </main>
+        </div>
+    );
+};
+
+export default HomeLayout;
