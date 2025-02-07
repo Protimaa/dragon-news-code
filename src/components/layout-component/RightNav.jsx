b7d4a14a8c2dfd06892f362f833643679ff92f1b
Protima <protima.swe@gmail.com>
2025-02-07 18:33:54 +0800

    left asidebar successfully done

---
+++ src/components/layout-component/RightNav.jsx
@@ -0,0 +1,12 @@
+import SocialLogin from "../SocialLogin";
+
+
+const RightNav =() => {
+    return (
+        <div>
+            <SocialLogin></SocialLogin>
+        </div>
+    );
+};
+
+export default RightNav;
