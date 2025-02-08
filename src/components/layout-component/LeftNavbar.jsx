b55e1daeab226c50dff9e7d4fa5bd5b68ecb93fe
Protima <protima.swe@gmail.com>
2025-02-08 08:03:02 +0800

    home page right asidebar successfully done

--- src/components/layout-component/LeftNavbar.jsx
+++ src/components/layout-component/LeftNavbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 
 const LeftNavbar = () => {
@@ -13,11 +14,16 @@ const LeftNavbar = () => {
         <div>
             <h2 className="font-semibold">All Category {categories.length}</h2>
             <div className="flex flex-col gap-2 mb-3">
+                
                 {categories.map((category) => (
-                    <button className="btn bg-base-100 border-none" key={category.category_id}>
+
+                    <NavLink 
+                    to={`/category/${category.category_id}`} 
+                    className="btn bg-base-100 border-none" key={category.category_id}>
                         {category.category_name}
-                    </button>
+                    </NavLink>
                 ))}
+
             </div>
         </div>
     );
--- src/components/layout-component/RightNav.jsx
+++ src/components/layout-component/RightNav.jsx
@@ -1,10 +1,12 @@
+import FindUs from "../FindUs";
 import SocialLogin from "../SocialLogin";
 
 
 const RightNav =() => {
     return (
-        <div>
+        <div className="space-y-5">
             <SocialLogin></SocialLogin>
+            <FindUs></FindUs>
         </div>
     );
 };
