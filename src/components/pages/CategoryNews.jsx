7b150c550ba89e970f7e738409b25f6c6bd7f586
Protima <protima.swe@gmail.com>
2025-02-08 11:21:58 +0800

    Home page main section successfully done

--- src/components/pages/CategoryNews.jsx
+++ src/components/pages/CategoryNews.jsx
@@ -1,12 +1,23 @@
 import { useLoaderData } from "react-router-dom";
+import NewsCard from "../NewsCard";
 
 
 const CategoryNews = () => {
-    const data = useLoaderData();
-    console.log(data);
+    const {data : news} = useLoaderData();
+    
     return (
         <div>
-            Category page
+            <h2 className="font-semibold mb-3">Dragon News Home</h2>
+            <p className="text-gray-400 text-sm">
+                {news.length} News Found On This Category
+            </p>
+            <div>
+                {
+                    news.map((singleNews) => (
+                        <NewsCard key={singleNews._id} news={singleNews}></NewsCard>
+                    ))
+                }
+            </div>
         </div>
     );
 };
